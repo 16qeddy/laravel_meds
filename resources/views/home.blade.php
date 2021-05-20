8f54af78f3e5a8ac766e21893d9a1e931f36c283
Charles <16qeddy@gmail.com>
2021-05-20 15:55:25 -0500

    setting up for deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,5 @@
     <body class="antialiased">
         <div id="app"></div>
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ secure_assets('js/app.js') }}?time={{ time() }}"></script>
     </body>
 </html>
