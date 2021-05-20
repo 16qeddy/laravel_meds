11ae3a35c918eb77da9d4c86b7e4203b80873436
Charles <16qeddy@gmail.com>
2021-05-20 16:16:37 -0500

    setting up for deployment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{secure_asset('/css/app.css')}}" rel="stylesheet">
+        <link href="{{env('APP_ENV') === 'local'? asset('/css/app.css') : secure_asset('/css/app.css')}}" rel="stylesheet">
         <title>Medication Lookup</title>
     </head>
     <body class="antialiased">
         <div id="app"></div>
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="{{ env('APP_ENV') === 'local'? asset('js/app.js') : secure_asset('js/app.js') }}" defer></script>
     </body>
 </html>
